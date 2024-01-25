643bee6265312759779f5914a5d51e2598719603
rgrunberg <33644220+rgrunberg@users.noreply.github.com>
2024-01-25 10:32:34 -0500

    add types

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,4 +1,7 @@
-export const log = (eventName, eventProperties) => {
+export const log = (
+  eventName: string,
+  eventProperties: { [index: string]: string | number }
+) => {
   // If this were a real app, we'd send this data to an analytics service
   const obj = { eventName, ...eventProperties };
 };
--- lib/featureFlags.ts
+++ lib/featureFlags.ts
@@ -1,9 +1,9 @@
-export const evaluate = (flagName) => {
+export const evaluate = (flagName: string) => {
   // If this were a real app, we'd fetch this data from a remote service
   // Instead just return true or false at random
-  const validExperimentNames = ['buttonColor'];
-  if (validExperimentNames.contains(flagName) === false) {
-    throw new Error('Experiment name not found');
+  const validExperimentNames = ["buttonColor"];
+  if (validExperimentNames.indexOf(flagName) == -1) {
+    throw new Error("Experiment name not found");
   }
   const array = [true, false];
   return array[Math.floor(Math.random() * array.length)];
