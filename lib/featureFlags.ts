a9763d29ca7198e379642857a790edfdab201401
rgrunberg <33644220+rgrunberg@users.noreply.github.com>
2024-04-05 10:30:01 -0700

    update var name

--- lib/featureFlags.ts
+++ lib/featureFlags.ts
@@ -1,9 +1,9 @@
 export const evaluate = (flagName: string, id: string) => {
   // If this were a real app, we'd fetch this data from a remote service
   // Instead just return true or false at random
-  const validExperimentNames = ["buttonColor"];
-  if (validExperimentNames.indexOf(flagName) == -1) {
-    throw new Error("Experiment name not found");
+  const validFlagNames = ["buttonColor"];
+  if (validFlagNames.indexOf(flagName) == -1) {
+    throw new Error("Flag name not found");
   }
   const array = ["#e5e7eb", "#bfdbfe"];
   return array[Math.floor(Math.random() * array.length)];
