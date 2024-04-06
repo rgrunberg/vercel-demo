af9f2f13d7685b8505780390351f67165601c0ae
rgrunberg <33644220+rgrunberg@users.noreply.github.com>
2024-04-05 18:12:35 -0700

    part 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 import Image from "next/image";
+import { evaluate } from "../lib/featureFlags";
+import { getId, log } from "../lib/analytics";
 
 export default function Home() {
   const subscribe = () => {
     const email = prompt("Enter your email address to subscribe");
+    log("subscribeButtonClicked", { id: getId() });
+    if (email) {
+      log("subscribeEmailSubmitted", { id: getId() });
+    }
   };
-
+  const experimentValue = evaluate("buttonColor", getId());
+  log("buttonColorExperimentExposure", { id: getId(), color: experimentValue });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <button
           className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
           onClick={subscribe}
+          style={{
+            backgroundColor: experimentValue,
+          }}
         >
           Subscribe for updates
         </button>
