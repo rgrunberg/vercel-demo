c0bb4c041fd523d32f974027dfbe997a697f7b5d
rgrunberg <33644220+rgrunberg@users.noreply.github.com>
2024-04-09 09:50:54 -0700

    exp ;

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,12 +1,42 @@
+import { getVariant } from "../lib/helpers";
+
 export const log = (
   eventName: string,
   eventProperties: { [index: string]: string | number | boolean }
 ) => {
   // If this were a real app, we'd send this data to an analytics service
   const obj = { eventName, ...eventProperties };
+  console.log(obj);
 };
 
 export const getId = () => {
   // A fake id
   return Date.now().toString(36) + Math.random().toString(36).substring(2);
+};
+
+const experiments: { experimentName: string; variants: string[] }[] = [
+  {
+    experimentName: "buttonColor",
+    variants: ["#e5e7eb", "#bfdbfe"],
+  },
+];
+
+const experimentNames = experiments.map((experiment) => {
+  return experiment.experimentName;
+});
+
+export const experiment = (experimentName: string, id: string) => {
+  // Check the experiment exists
+  const experiment = experiments.find(
+    (experiment) => experiment.experimentName == experimentName
+  );
+  if (!experiment) {
+    throw new Error("Experiment not found");
+  }
+  // Get the variant for this experiment
+  const variant = getVariant(experiment.variants, id);
+  // Log the exposure
+  log("experimentExposure", { id, experimentName, variant });
+  // Return the experiment value
+  return variant;
 };
